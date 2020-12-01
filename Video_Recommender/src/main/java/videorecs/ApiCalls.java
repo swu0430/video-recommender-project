bae08b9b65710356cc31f5d0e62e7ca3adabdf41
Steven Wu <swu0430@gmail.com>
2020-12-01 15:15:20 -0500

    Added another Api Call for video categories

--- Video_Recommender/src/main/java/videorecs/ApiCalls.java
+++ Video_Recommender/src/main/java/videorecs/ApiCalls.java
@@ -4,13 +4,10 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.SearchListResponse; //for the YouTube "Search: list" API
+import com.google.api.services.youtube.model.VideoCategoryListResponse; //for the YouTube "VideoCategories: list" API
 import com.google.api.services.youtube.model.VideoListResponse; //for the YouTube "Videos: list" API
 
 /**
@@ -37,23 +34,23 @@ public class ApiCalls {
 	 * @throws GoogleJsonResponseException
 	 */
 	public SearchListResponse videoSearches(YouTube youtubeService, String devKey, ArrayList<String> part, 
-			long maxResults, String pageToken, String query, ArrayList<String> type, String duration) 
-			throws GeneralSecurityException, IOException, GoogleJsonResponseException {
-			
-	        // Define and execute the "Search: list" API request
-	        YouTube.Search.List request = youtubeService.search()
-	        	.list(part);	
-	        SearchListResponse response = request.setKey(devKey)
-	            .setMaxResults(maxResults)
-	            .setPageToken(pageToken)
-	        	.setQ(query)
-	        	.setType(type)
-	        	.setVideoDuration(duration)
-	            .execute();
-	        
-	        // Return the "Search: list" API JSON
-	        return response;
-		}
+		long maxResults, String pageToken, String query, ArrayList<String> type, String duration) 
+		throws GeneralSecurityException, IOException, GoogleJsonResponseException {
+		
+        // Define and execute the "Search: list" API request
+        YouTube.Search.List request = youtubeService.search()
+        	.list(part);	
+        SearchListResponse response = request.setKey(devKey)
+            .setMaxResults(maxResults)
+            .setPageToken(pageToken)
+        	.setQ(query)
+        	.setType(type)
+        	.setVideoDuration(duration)
+            .execute();
+        
+        // Return the "Search: list" API JSON
+        return response;
+	}
 	
 	/**
 	 * This method calls the YouTube "Videos: list" API to produce a JSON list of more drilled-down detail
@@ -67,17 +64,43 @@ public class ApiCalls {
 	 * @throws IOException
 	 * @throws GoogleJsonResponseException
 	 */
-	public VideoListResponse videoSearches(YouTube youtubeService, String devKey, ArrayList<String> part, 
-			ArrayList<String> id) throws GeneralSecurityException, IOException, GoogleJsonResponseException {
+	public VideoListResponse videoDetails(YouTube youtubeService, String devKey, ArrayList<String> part, 
+		ArrayList<String> id) throws GeneralSecurityException, IOException, GoogleJsonResponseException {
+	
+		// Define and execute the "Videos: list" API request
+	    YouTube.Videos.List request = youtubeService.videos()
+	    	.list(part);	
+	    VideoListResponse response = request.setKey(devKey)
+	    	.setId(id)
+	        .execute();
+	    
+	    // Return the "Videos: list" API JSON
+	    return response;
+	}
+	
+	/**
+	 * This method calls the YouTube "VideoCategories: list" API to produce a JSON of the full list of 
+	 * video categories for YouTube videos in the US region.
+	 * @param youtubeService - authorized YouTube API client service
+	 * @param devKey - developer key needed for YouTube API client service authorization
+	 * @param part - ArrayList of Strings; should be set to "content details" and "statistics"
+	 * @param regionCode of the country the user lives in (should be set to "us")
+	 * @return JSON for the YouTube "VideoCategories: list" API
+	 * @throws GeneralSecurityException
+	 * @throws IOException
+	 * @throws GoogleJsonResponseException
+	 */
+	public VideoCategoryListResponse videoCategories(YouTube youtubeService, String devKey, ArrayList<String> part,
+		String regionCode) throws GeneralSecurityException, IOException, GoogleJsonResponseException {
 		
-			// Define and execute the "Videos: list" API request
-		    YouTube.Videos.List request = youtubeService.videos()
-		    	.list(part);	
-		    VideoListResponse response = request.setKey(devKey)
-		    	.setId(id)
-		        .execute();
-		    
-		    // Return the "Videos: list" API JSON
-		    return response;
-		}
+		// Define and execute the "VideoCategories: list" API request
+	    YouTube.VideoCategories.List request = youtubeService.videoCategories()
+	    	.list(part);	
+	    VideoCategoryListResponse response = request.setKey(devKey)
+	    	.setRegionCode(regionCode)
+	        .execute();
+	    
+	    // Return the "VideoCategories: list" API JSON
+	    return response;
+	}
 }
--- Video_Recommender/src/main/java/videorecs/Control.java
+++ Video_Recommender/src/main/java/videorecs/Control.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
@@ -13,6 +14,7 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
 import com.google.api.services.youtube.model.VideoListResponse;
 
 /**
@@ -75,13 +77,16 @@ public class Control {
     	ArrayList<String> partSearchList = new ArrayList<String>();
     	partSearchList.add("snippet");
     	
-    	// number of search results per page (max is 50)
-    	long maxResults = 50L;
+    	// number of search results per page (max is 50, but results may get funky that high)
+    	long maxResults = 30L;
     	
     	// token for next page of searches if needed (set initial value to null)
     	String pageToken = null;
     	
-    	// search query
+    	// search query. ***Consider filtering by key words such as "how to", "tutorial", "demo", "tips",
+    	// "beginner", "intermediate", "advanced", "learn", "easy", "hard", "great for", "try", "education",
+    	// "like a pro", "like a boss", "hack", in (1)title (2)categories (3)description rather than just 
+    	//adding "how to" in the query itself***
     	System.out.print("Enter an activity: ");
     	String query = scanner.nextLine();
     	query += " how to";
@@ -95,9 +100,16 @@ public class Control {
     	String duration = scanner.nextLine();
     	System.out.println();
     	
+    	// Call the YouTube "Search: list" API to search for a set of videos based on criteria above
     	SearchListResponse videoSearchResults = apiCalls.videoSearches(youtubeService, DEVELOPER_KEY, 
     		partSearchList, maxResults, pageToken, query, type, duration);
-        	
+        
+      
+        List<SearchResult> items = videoSearchResults.getItems();
+        for (SearchResult sr : items) {
+        	System.out.println(sr.getId().getVideoId());
+        }
+    	
         // Print out the SearchList JSON
         // (Eventually don't want to print this out, want to be able to extract values from it behind the scenes)
         System.out.print("YouTube Search List API JSON: ");
@@ -116,13 +128,20 @@ public class Control {
     	ArrayList<String> partVideosList = new ArrayList<String>();
     	partVideosList.add("contentDetails");
     	partVideosList.add("statistics");
+    	partVideosList.add("snippet");
         
     	// id --> set to the video id
         //ArrayLists added by SW
         ArrayList<String> id = new ArrayList<String>();
         id.add("ePm165e4c1E"); //FILLER --> to be filled in once we figure how to pull the video ID from SearchList JSON
         
-        VideoListResponse videoDetails = apiCalls.videoSearches(youtubeService, DEVELOPER_KEY, partVideosList, id);
+        // Call the YouTube "Videos: list" API to filter video searches by further criteria and pick out
+        // final recommendations for the user
+        VideoListResponse videoDetails = apiCalls.videoDetails(youtubeService, DEVELOPER_KEY, partVideosList, id);
+//        List<Video> items = videoDetails.getItems();
+//        for (Video v : items) {
+//        	v.
+//        }
         
         // Print out the VideosList JSON
         // (Eventually don't want to print this out, want to be able to extract values from it behind the scenes)
